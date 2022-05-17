3c2ae305336d3a947c909bbe1391e3c8305e8aa4
Spencer Johnson <sljohnson2222@gmail.com>
2022-05-17 15:22:57 -0500

    added contact me section with links

--- portfolio-website/src/components/Contact.js
+++ portfolio-website/src/components/Contact.js
@@ -3,7 +3,39 @@ import React from "react";
 export default function Contact() {
     return (
         <div>
-            
+            <h2> Contact Information</h2>
+            <div>
+            <div className="bio-detail">
+                <h5>Name</h5>
+                <h5>-</h5>
+                <p>Spencer Johnson</p>
+            </div>
+            <div className="bio-detail">
+                <h5>Phone</h5>
+                <h5>-</h5>
+                <p>417-209-5022</p>
+            </div>
+            <div className="bio-detail">
+                <h5>Email</h5>
+                <h5>-</h5>
+                <a href="mailto: sljohnson2222@gmail.com">sljohnson2222@gmail.com</a>
+            </div>
+            <div className="bio-detail">
+                <h5>LinkedIn</h5>
+                <h5>-</h5>
+                <a href="https://www.linkedin.com/in/slj2222/">https://www.linkedin.com/in/slj2222/</a>
+            </div>
+            <div className="bio-detail">
+                <h5>GitHub</h5>
+                <h5>-</h5>
+                <a href="https://github.com/slj2222">https://github.com/slj2222</a>
+            </div>
+            <div className="bio-detail">
+                <h5>Dev.to</h5>
+                <h5>-</h5>
+                <a href="https://dev.to/slj2222">https://dev.to/slj2222</a>
+            </div>
+            </div>
         </div>
     )
 }
--- portfolio-website/src/components/LandingPage.js
+++ portfolio-website/src/components/LandingPage.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function LandingPage() {
     return (
         <div className="landing-page">
-            ~Landing Page~
             <h3>
                 I am Spencer Johnson and welcome to my page!
             </h3>
